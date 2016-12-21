4619c50a1a86970e229bf114b6201f2b28515d24
Roan Brand <roan.brand@imqs.co.za>
2016-12-21 11:42:59 +0200

    log first stock

--- service.go
+++ service.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"github.com/RoanBrand/EquityTracker/scheduler"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -20,9 +25,18 @@ type stockDetails struct {
 
 func main() {
 	w := sync.WaitGroup{}
-	scheduler := scheduler.NewScheduler(time.Minute, func(now time.Time) {
+	scheduler := scheduler.NewScheduler(time.Minute*15, func(now time.Time) {
 		results := getStockPrices([]string{"EOH"})
-		log.Printf("%v @ %v", results, now)
+		first := results[0]
+		//log.Printf("%v @ %v\n", first, now)
+		numPrice, err := strconv.ParseFloat(strings.Replace(first.Price, ",", "", -1), 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//log.Println(numPrice)
+		if err := insertRecord(first.Name, now, first.Id, numPrice); err != nil {
+			log.Fatal(err)
+		}
 	})
 	defer scheduler.Stop()
 
@@ -45,3 +59,42 @@ func getStockPrices(names []string) []stockDetails {
 	json.Unmarshal(queryOutput.Bytes(), &results)
 	return results
 }
+
+func checkTable() error {
+	db, err := sql.Open("postgres", "user=imqs password=1mq5p@55w0rd dbname=stocks sslmode=disable")
+	if err != nil {
+		return err
+	}
+	rows, err := db.Query(`SELECT "TIMESTAMP", "PRICE" FROM "EOH" LIMIT 1`)
+	var ts time.Time
+	var price float64
+	for rows.Next() {
+		rows.Scan(&ts, &price)
+		log.Println(ts, price)
+	}
+
+	return nil
+}
+
+func insertRecord(tableName string, ts time.Time, id string, price float64) error {
+	db, err := sql.Open("postgres", "user=imqs password=1mq5p@55w0rd dbname=stocks sslmode=disable")
+	if err != nil {
+		return err
+	}
+	cleanTs := time.Date(ts.Year(), ts.Month(), ts.Day(), ts.Hour(), ts.Minute(), ts.Second(), 0, ts.Location())
+	_, err = db.Exec(fmt.Sprintf(`INSERT INTO "%s" ("TIMESTAMP", "ID", "PRICE") VALUES ($1, $2, $3)`, tableName), cleanTs.Format(time.RFC3339), id, price)
+	return err
+}
+
+// CREATE TABLE "EOH"
+// (
+// rowid serial NOT NULL,
+// "TIMESTAMP" timestamp without time zone,
+// "ID" character varying,
+// "PRICE" double precision
+// )
+// WITH (
+// OIDS=FALSE
+// );
+// ALTER TABLE "EOH"
+// OWNER TO imqs;
